622d4967792c8a355edf8cab7af50bf9393afe72
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2016-09-18 21:54:57 +0700

    pausing the timer

--- main.js
+++ main.js
@@ -8,23 +8,16 @@ function createWindow() {
   win.on('closed', () => { win = null; });
 }
 
-
-
-
 function createTrayIcon() {
-
-  win.webContents.executeJavaScript('makeImage(0)', false, (dataURL) => {
+  win.webContents.executeJavaScript('makeImage()', false, (dataURL) => {
     tray = new Tray(nativeImage.createFromDataURL(dataURL));
 
     tray.setContextMenu(Menu.buildFromTemplate([
       { label: 'exit', click() { app.quit() } }
     ]));  
 
-    tray.on('click', () => {
-      startTimer(); //> toggle timer instead
-    });
+    tray.on('click', () => timer.toggle());
   })
-
 }
 
 app.on('ready', () => { createWindow(); createTrayIcon() });
@@ -36,25 +29,55 @@ app.on('activate', () => { if (!win) createWindow(); });
 /////////////////////////////////
 
 
-function updateIcon(value) {
-  win.webContents.executeJavaScript(`makeImage(${value})`, false, (dataURL) => {
+
+
+function updateIcon() {
+  let json = JSON.stringify(timer);
+  win.webContents.executeJavaScript(`makeImage(${json})`, false, (dataURL) => {
     tray.setImage(nativeImage.createFromDataURL(dataURL));
   });
 }
 
-function startTimer() {
-  console.log('ok');
-  let startTime = Date.now();
-  let secondsToGo = 20 * 60;
-  setInterval(() => {
-    const spent = Math.round((Date.now() - startTime) / 1000); // seconds
-    const left = secondsToGo - spent;
-    console.log(left);
-    updateIcon(left);
-  }, 1000);
 
+
+class Timer {
+
+  constructor() {
+    this.started = false;
+    this.intervalId = null;
+    this.secondsToGo = 20 * 60;
+    this.secondsLeft = this.secondsToGo;
+  }
+
+  toggle() {
+    if (!this.started) this.start();
+    else this.pause();
+  }
+
+  start() {
+    this.started = true;
+    updateIcon();
+
+    let startTime = Date.now();
+    this.intervalId = setInterval(() => {
+      const spent = Math.round((Date.now() - startTime) / 1000); // seconds
+      this.secondsLeft = this.secondsToGo - spent;
+      updateIcon();
+    }, 1000);
+  }
+
+  pause() {
+    this.started = false;
+    clearInterval(this.intervalId);
+    updateIcon();
+  }
 
 }
+
+let timer = new Timer();
+
+
+
 
 
 
